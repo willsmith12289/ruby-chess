0fedfdc256406ef5a31fd2618e4e3abacb79f6af
will <willsmith12289@gmail.com>
2017-09-25 09:49:22 -0400

    working again

--- king.rb
+++ king.rb
@@ -5,55 +5,21 @@ class King < Piece
   end
  
 #################################
-# Creates possible moves array
-# adds all possible moves to it
-#### 
-  def moves(this)
-    row = this.row.to_i
-    column = this.column.to_i
-    poss_moves = Array.new()
-    poss_moves << [row+1, column]
-    poss_moves << [row-1, column]
-    poss_moves << [row, column-1]
-    poss_moves << [row, column+1]
-    poss_moves << [row+1, column+1]
-    poss_moves << [row-1, column+1]
-    poss_moves << [row-1, column-1]
-    poss_moves << [row+1, column-1]
-  end
-
-#################################
 # Checks legality of king move coords
 # loops through possible moves to see if contains move coords 
 #### 
   def legal(this, coords)
-    @coords_x = coords[0].to_i
-    @coords_y = coords[1].to_i
-    @moves = moves(this)
-    @moves.each do |move|
-      if @coords_x === move[0].to_i && @coords_y === move[1].to_i
-        occupied = this.occupied?(coords)
-        return occupied
-      else
-        next
-      end
+    row_dist = (this.row.to_i - coords[0].to_i).abs
+    col_dist = (this.column.to_i - coords[1].to_i).abs
+    if row_dist > 1 || col_dist > 1
+      return false
+    else
+      occupied = this.occupied?(coords)
+      return occupied
     end
-    return false
   end
 
 #################################
 #
 #### 
-  # def check
-  #   if 
-  # end
-
-#################################
-#
-#### 
-  # def check_mate
-  #   @moves = moves(self)
-  #   @moves.each do |move|
-  # end
-
 end
--- queen.rb
+++ queen.rb
@@ -8,20 +8,7 @@ class Queen < Piece
 # Checks legality of Queens moves
 ####
   def legal(this, coords)
-    row = this.row.to_i
-    column = this.column.to_i
-    dest_row = coords[0].to_i
-    dest_col = coords[1].to_i
-    row_dist = (row - dest_row).abs
-    col_dist = (column - dest_col).abs
-    puts row_dist
-    puts col_dist
-    if row_dist > 1 || col_dist > 1
-      return false
-    else
-      occupied = this.occupied?(coords)
-      return occupied
-    end
+    return true
   end
 
 end
